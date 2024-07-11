50c839bbd6c7569d1a3feeeed056c48cc0bd01fe
Thomas Christensen <thomas@chrstnsn.dk>
2024-07-11 07:59:18 +0200

    wifi

--- src/main.rs
+++ src/main.rs
@@ -8,10 +8,27 @@ use embedded_hal::digital::{InputPin, OutputPin};
 use esp_backtrace as _;
 use esp_hal::gpio::any_pin::AnyPin;
 use esp_hal::gpio::{Io, Level, OutputOpenDrain, Pull};
+use esp_hal::rng::Rng;
+use esp_hal::timer::timg::TimerGroup;
 use esp_hal::{
     clock::ClockControl, delay::Delay, peripherals::Peripherals, prelude::*, system::SystemControl,
+    timer::PeriodicTimer,
 };
 use one_wire_bus::{OneWire, OneWireResult};
+
+use esp_wifi::{
+    current_millis,
+    wifi::{
+        utils::create_network_interface, AccessPointInfo, ClientConfiguration, Configuration,
+        WifiError, WifiStaDevice,
+    },
+    wifi_interface::WifiStack,
+    EspWifiInitFor,
+};
+use smoltcp::{
+    iface::SocketStorage,
+    wire::{IpAddress, Ipv4Address},
+};
 
 fn find_devices<P, E>(delay: &mut impl DelayNs, one_wire_pin: P)
 where
@@ -89,13 +106,79 @@ fn main() -> ! {
     let clocks = ClockControl::max(system.clock_control).freeze();
     let mut delay = Delay::new(&clocks);
 
-    let io = Io::new(peripherals.GPIO, peripherals.IO_MUX);
+    esp_println::logger::init_logger_from_env();
 
+    // gpio4 is the default pin for the one wire bus
+    let io = Io::new(peripherals.GPIO, peripherals.IO_MUX);
     let mut pin = AnyPin::new(io.pins.gpio4);
     let mut od = OutputOpenDrain::new(pin, Level::High, Pull::None);
-    // let mut pin = io.pins.gpio4;
 
-    esp_println::logger::init_logger_from_env();
+    // wifi
+    let timer = TimerGroup::new(peripherals.TIMG1, &clocks, None).timer0;
+    let init = esp_wifi::initialize(
+        EspWifiInitFor::Wifi,
+        timer,
+        Rng::new(peripherals.RNG),
+        peripherals.RADIO_CLK,
+        &clocks,
+    )
+    .unwrap();
+
+    let wifi = peripherals.WIFI;
+    let mut socket_set_entries: [SocketStorage; 3] = Default::default();
+    let (iface, device, mut controller, sockets) =
+        create_network_interface(&init, wifi, WifiStaDevice, &mut socket_set_entries).unwrap();
+    let wifi_stack = WifiStack::new(iface, device, sockets, current_millis);
+
+    let client_config = Configuration::Client(ClientConfiguration {
+        ssid: SSID.try_into().unwrap(),
+        password: PASS.try_into().unwrap(),
+        ..Default::default()
+    });
+    let res = controller.set_configuration(&client_config);
+    log::info!("wifi_set_configuration returned {:?}", res);
+
+    controller.start().unwrap();
+    log::info!("is wifi started: {:?}", controller.is_started());
+
+    log::info!("Start Wifi Scan");
+    let res: Result<(heapless::Vec<AccessPointInfo, 10>, usize), WifiError> = controller.scan_n();
+    if let Ok((res, _count)) = res {
+        for ap in res {
+            log::info!("{:?}", ap);
+        }
+    }
+
+    log::info!("{:?}", controller.get_capabilities());
+    log::info!("wifi_connect {:?}", controller.connect());
+
+    // wait to get connected
+    log::info!("Wait to get connected");
+    loop {
+        let res = controller.is_connected();
+        match res {
+            Ok(connected) => {
+                if connected {
+                    break;
+                }
+            }
+            Err(err) => {
+                log::error!("{:?}", err);
+            }
+        }
+    }
+    log::info!("{:?}", controller.is_connected());
+
+    // wait for getting an ip address
+    log::info!("Wait to get an ip address");
+    loop {
+        wifi_stack.work();
+
+        if wifi_stack.is_iface_up() {
+            log::info!("got ip {:?}", wifi_stack.get_ip_info());
+            break;
+        }
+    }
 
     find_devices(&mut delay, &mut od);
 
